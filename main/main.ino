f04abe69a71ba6af72913cc464e88826149863aa
Akshay Kannan <akshayk@google.com>
2022-04-19 22:33:32 -0700

    Added callback handling code

--- main/main.ino
+++ main/main.ino
@@ -13,6 +13,32 @@ bool currentState = false;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+void callback(char* topic, byte* message, unsigned int length) {
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char) message[i]);
+    messageTemp += (char) message[i];
+  }
+  Serial.println();
+
+  if (String(topic) == mqtt_sub_topic) {
+    Serial.print("Changing output to ");
+    if (messageTemp == "ON") {
+      Serial.println("ON");
+      currentState = true;
+      digitalWrite(led_pin, HIGH);
+    } else if (messageTemp == "OFF") {
+      Serial.println("OFF");
+      currentState = false;
+      digitalWrite(led_pin, LOW);
+    }
+  }
+}
+
 void setup() {
   Serial.begin(9600);
 
@@ -28,7 +54,7 @@ void setup() {
 
   //Initialize MQTT
   client.setServer(mqtt_server, mqtt_port);
-
+  client.setCallback(callback);
   client.publish(mqtt_topic, "OFF");
 
   //Initialize pins
@@ -42,12 +68,35 @@ void toggle() {
   client.publish(mqtt_topic, currentState ? "ON" : "OFF");
 }
 
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Subscribe
+      client.subscribe("esp32/output");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
   int reading = digitalRead(button_pin);
   if (reading != lastButtonState) {
     lastDebounceTime = millis();
   }
+  
   if ((millis() - lastDebounceTime) > debounceDelay) {
     if (reading != buttonState) {
       buttonState = reading;
